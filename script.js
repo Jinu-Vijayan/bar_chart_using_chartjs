04cb86b723b0de08c7d55551475d95856298a8e6
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-01-31 20:11:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const MY_CHART = document.querySelector("#myChart");
+
+// const LABELS = Utils.months({count: 7});
+const data = {
+  labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul"],
+  datasets: [{
+    label: 'Price in rs',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: "blue",
+    borderWidth: 1
+  }]
+};
+
+const CONFIG = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+};
+
+// console.log(Chart)
+
+new Chart(MY_CHART,CONFIG);
